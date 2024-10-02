eafa2473171d0d832c3187ea2b3e720d05caf26a
Rodolfo Silva de Souza <sdesouza.rodolfo@gmail.com>
2024-10-02 19:16:06 -0300

    feat: ajustes na app

--- app.py
+++ app.py
@@ -6,42 +6,43 @@ def main():
     # Configurar o layout da página
     st.set_page_config(layout="wide")
 
-    st.title("Acompanhamento de estrategias")
+    st.title("Acompanhamento de estratégias")
 
+    # Carregar o dataset
     df = pd.read_csv("estrategias.csv", sep=";", decimal=",")
 
+    # Filtrar os dados para a estratégia 'cadastro de professores'
     consulta = df.loc[df['nomeEstrategia'] == 'cadastro de professores']
     consulta.set_index('index', inplace=True)
 
+    # Selecionar colunas relevantes
     selected_columns = consulta[['mes', 'valor', 'nomeEstrategia']]
 
-    st.markdown("Abaixo os gráficos de acompanhamento das estrategias aplicadas aos indicadores da meta 15 do PNE.")
+    st.markdown("Abaixo os gráficos de acompanhamento das estratégias aplicadas aos indicadores da meta 15 do PNE.")
 
+    # Descrição do indicador 15A
     multi = '''**INDICADOR 15A** - Proporção de docências da educação infantil com professores cuja formação superior está adequada à área de conhecimento que lecionam.'''
     st.markdown(multi)
 
+    # Criar o gráfico
     fig = px.line(selected_columns, x='mes', y='valor', color='nomeEstrategia', title='Estrategia 2')
-
     st.plotly_chart(fig)
-    col1, col2, col3 = st.columns(3)
 
+    # Exibir os gráficos em colunas
+    col1, col2, col3 = st.columns(3)
     with col1:
         st.plotly_chart(fig)
-
     with col2:
         st.plotly_chart(fig)
-
     with col3:
         st.plotly_chart(fig)
 
-    multi = '''**INDICADOR 15B** - Proporção de docências dos anos iniciais do ensino fundamental com professores cuja formação superior está adequada à área de conhecimento que lecionam.'''
-    st.markdown(multi)
+    # Outros indicadores
+    st.markdown('''**INDICADOR 15B** - Proporção de docências dos anos iniciais do ensino fundamental com professores cuja formação superior está adequada à área de conhecimento que lecionam.''')
 
-    multi = '''**INDICADOR 15C** - Proporção de docências dos anos finais do ensino fundamental com professores cuja formação superior está adequada à área de conhecimento que lecionam.'''
-    st.markdown(multi)
+    st.markdown('''**INDICADOR 15C** - Proporção de docências dos anos finais do ensino fundamental com professores cuja formação superior está adequada à área de conhecimento que lecionam.''')
 
-    multi = '''**INDICADOR 15D** - Proporção de docências do ensino médio com professores cuja formação superior está adequada à área de conhecimento que lecionam.'''
-    st.markdown(multi)
+    st.markdown('''**INDICADOR 15D** - Proporção de docências do ensino médio com professores cuja formação superior está adequada à área de conhecimento que lecionam.''')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
