7d2d4648f236977e29b0cdc880f65e8738541d24
Rodolfo Silva de Souza <sdesouza.rodolfo@gmail.com>
2024-11-28 22:36:23 -0300

    feat: ajustes nos dashboard

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import plotly.express as px
 from st_files_connection import FilesConnection
+import pandas as pd
 
 def main():
     # Configurar o layout da página
@@ -11,6 +12,14 @@ def main():
 
     # Especificar o caminho do arquivo no bucket do GCS
     df = conn.read("monitora_pne_15_streamlit/estrategias.csv", input_format="csv", ttl=600, sep=";", decimal=",")
+
+    # Ordenar os meses corretamente
+    df["mes"] = pd.Categorical(
+        df["mes"],
+        categories=["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"],
+        ordered=True
+    )
+    df = df.sort_values(by=["ano", "mes"])  # Ordenar por ano e mês
 
     # Agrupar os dados por indicador
     grouped = df.groupby("indicador")
@@ -35,8 +44,11 @@ def main():
                 x="mes",
                 y="valor",
                 title=f"{estrategia}",
-                markers=True
+                markers=True,
+                line_shape='linear',
+                labels={"mes": "Mês", "valor": "Valor"}
             )
+            grafico.update_xaxes(categoryorder="array", categoryarray=["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"])
 
             # Mostrar os gráficos em colunas alternadas
             if i % 2 == 0:
